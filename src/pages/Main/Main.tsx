b1be65cf439678b0a12bb0c7c85146b893755ab4
Ki Do <65141066+kido-c@users.noreply.github.com>
2023-09-12 19:09:16 +0900

    chore: redux initial setting

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,6 +1,26 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import Counter from '@/components/molcules/Counter'
+import { RootState } from 'src/stores'
+import { decrease, increase, increaseBy } from '@/stores/counter'
 
 const Main = () => {
+  // 상태를 조회합니다. 상태를 조회 할 때에는 state 의 타입을 RootState 로 지정해야합니다.
+  const count = useSelector((state: RootState) => state.counter.count)
+  const dispatch = useDispatch() // 디스패치 함수를 가져옵니다
+
+  // 각 액션들을 디스패치하는 함수들을 만들어줍니다
+  const onIncrease = () => {
+    dispatch(increase())
+  }
+
+  const onDecrease = () => {
+    dispatch(decrease())
+  }
+
+  const onIncreaseBy = (diff: number) => {
+    dispatch(increaseBy(diff))
+  }
   return (
     <div className='App'>
       <h1>React Boilerplate with webpack</h1>
@@ -13,8 +33,16 @@ const Main = () => {
           <li>prettier</li>
           <li>storybook</li>
           <li>jest</li>
-          <li>hot reload test</li>
         </ul>
+      </section>
+      <section>
+        <div>redux 전역 상태 확인</div>
+        <Counter
+          count={count}
+          onIncrease={onIncrease}
+          onDecrease={onDecrease}
+          onIncreaseBy={onIncreaseBy}
+        />
       </section>
     </div>
   )
